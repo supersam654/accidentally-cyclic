00861b9cbd9e5a7ac80fc44ccaacfe65055196cb
Sam Marder <sam@marder.co>
2017-03-03 12:08:30 -0500

    Inspecting node_modules no longer shows all dependent node modules.

--- cyclic.js
+++ cyclic.js
@@ -15,10 +15,10 @@ const originalExit = process.exit
  *
  * These tend to be in a `node_modules` folder or not end with `.js`.
  */
-function looksLikeThirdParty (parentRelativePath, moduleRelativePath) {
-  return !moduleRelativePath.endsWith('.js') ||
-      moduleRelativePath.includes('node_modules') ||
-      parentRelativePath.includes('node_modules')
+function looksLikeThirdParty (parentPath, modulePath) {
+  return !modulePath.endsWith('.js') ||
+      modulePath.includes('node_modules') ||
+      parentPath.includes('node_modules')
 }
 
 function isModule (importName) {
@@ -29,13 +29,8 @@ function hijackLoad (visitor) {
   Module._load = function (request, parent, isMain) {
     const exports = originalLoad.apply(Module, arguments)
     const parentFullPath = parent.filename
-    let moduleFullPath
-    if (isModule(request)) {
-      moduleFullPath = request
-    } else {
-      moduleFullPath = Module._resolveFilename(request, parent)
-    }
-    visitor(parentFullPath, moduleFullPath)
+    const moduleFullPath = Module._resolveFilename(request, parent)
+    visitor(parentFullPath, moduleFullPath, request)
     return exports
   }
 }
@@ -60,33 +55,55 @@ function getCallerDirectory () {
   return path.dirname(callerFile)
 }
 
+function simplifyNames (originalRequest, dependencies, showNodeModules) {
+  const basePath = path.dirname(Module._resolveFilename(originalRequest, module))
+  const baseIsModule = isModule(originalRequest)
+  let simplifiedDependencies = []
+  // Make all paths, even on Windows, use forward slashes.
+  for (let dependency of dependencies) {
+    let parentPath = path.relative(basePath, dependency.parent).split(path.sep).join('/')
+    let modulePath
+    if (isModule(dependency.request)) {
+      // Use the value passed into `require`.
+      modulePath = dependency.request
+    } else {
+      // Use the path that the `require` got resolved into.
+      modulePath = path.relative(basePath, dependency.module).split(path.sep).join('/')
+    }
+
+    if (!showNodeModules && looksLikeThirdParty(parentPath, modulePath)) {
+      // Skip because this is obviously a node_module and we are supposed to skip those.
+      continue
+    }
+
+    if (!showNodeModules && baseIsModule && modulePath.startsWith('..')) {
+      // Skip because we originally imported an external module (which is in a node_modules) folder.
+      // This dependency is in a sibling directory so it is a different module.
+      continue
+    }
+    simplifiedDependencies.push({
+      parent: parentPath,
+      module: modulePath
+    })
+  }
+
+  return simplifiedDependencies
+}
+
 exports.require = function (entryPoint, showNodeModules) {
   // Fix relative paths
   // Don't touch absolute paths, core modules, and node_modules.
   if (entryPoint.startsWith('.')) {
     entryPoint = path.resolve(getCallerDirectory(), entryPoint)
-  } else if (!path.isAbsolute(entryPoint)) {
-    // It's not an absolute path either so it's a 3rd-party module.
-    // All imports will be discarded unless this is true because everything will be in a node_modules folder.
-    showNodeModules = true
   }
 
   let dependencies = []
-  // const basePath = Module._resolveFilename(entryPoint, process.cwd())
-  const basePath = path.dirname(path.resolve(getCallerDirectory(), entryPoint))
 
-  hijackLoad(function visitor (parentPath, modulePath) {
-    if (!showNodeModules && looksLikeThirdParty(parentPath, modulePath)) {
-      return
-    }
-
-    const parentRelativePath = path.relative(basePath, parentPath)
-    const moduleRelativePath = path.relative(basePath, modulePath)
-
-    // Make all paths, even on Windows, use forward slashes.
+  hijackLoad(function visitor (parentPath, modulePath, request) {
     dependencies.push({
-      parent: parentRelativePath.split(path.sep).join('/'),
-      module: moduleRelativePath.split(path.sep).join('/')
+      parent: parentPath,
+      module: modulePath,
+      request: request
     })
   })
 
@@ -94,6 +111,7 @@ exports.require = function (entryPoint, showNodeModules) {
     process.exit = function () {
       console.warn('Hijacked exit called!')
       // Something requested that we exit, so return the dependency graph first.
+      dependencies = simplifyNames(dependencies)
       resolve(dependencies)
     }
 
@@ -110,6 +128,8 @@ exports.require = function (entryPoint, showNodeModules) {
       process.exit = originalExit
       require.cache = originalCache
     }
+
+    dependencies = simplifyNames(entryPoint, dependencies, showNodeModules)
 
     // Get rid of the last known dependency because that is this file requiring the entry point.
     dependencies.pop()
